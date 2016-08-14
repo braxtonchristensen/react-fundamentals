61595cf085e89ef08c6bfd76769d8bf0c3895c0f
Braxton Christensen <braxton.christensen@gmail.com>
2016-08-14 11:33:31 -0600

    Enviorment State and Stateless Components Render Method

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Class Component
+//Has State
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+//Stateless Function Component
+//Statless
+// const App = () => <h1>Hello man</h1>
+
+export default App
