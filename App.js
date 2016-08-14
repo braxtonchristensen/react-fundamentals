e4579c17c1abe44138c3c5d091aa9922ad320b92
Braxton Christensen <braxton.christensen@gmail.com>
2016-08-14 12:00:30 -0600

    State Basics (what is state, how is state used)

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    //super gives us the context for 'this' within component
+    super();
+    this.state = {txt: 'this is teh state txt'}
+  }
+  update(event) {
+    this.setState({txt: event.target.value});
+  }
   render() {
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return (
+      <div>
+        <input type='text'
+          onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-},
-
-App.defaultProps = {
-  txt: 'this is default text'
-}
-
-ReactDOM.render(
-  <App cat={5} />, 
-  document.getElementById('app')
-);
 
 export default App
