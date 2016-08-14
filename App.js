a5ca16cb107e0dd98191549f761edef918c71816
Braxton Christensen <braxton.christensen@gmail.com>
2016-08-14 11:48:30 -0600

    intro to properties(defaultProps, propTypes, isRequired)

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-//Class Component
-//Has State
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
 }
 
-//Stateless Function Component
-//Statless
-// const App = () => <h1>Hello man</h1>
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+},
+
+App.defaultProps = {
+  txt: 'this is default text'
+}
+
+ReactDOM.render(
+  <App cat={5} />, 
+  document.getElementById('app')
+);
 
 export default App
