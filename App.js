b71c5520f89e2503d1dd9e7d965f1f2428b50672
Braxton Christensen <braxton.christensen@gmail.com>
2016-08-14 12:13:47 -0600

    Owner Ownee Relatioinship(create ownee component and chage value of state on composite component)

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+//Component (has state)
+//Composite component
+//Owner
 class App extends React.Component {
   constructor(){
     //super gives us the context for 'this' within component
     super();
     this.state = {txt: 'this is teh state txt'}
+    //add this so we can just call this.upate when ever we want to use this method
+    this.update = this.update.bind(this)
   }
   update(event) {
     this.setState({txt: event.target.value});
@@ -12,12 +17,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type='text'
-          onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     );
   }
 }
 
+//Stateless Component
+//Child Component
+//Ownee
+const Widget = (props) => {
+  return (
+    //changes the state.txt value on parent
+    <div>
+      <input type='text'
+        onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+}
 export default App
